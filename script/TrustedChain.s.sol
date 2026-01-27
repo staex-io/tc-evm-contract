8086898839fc477bb55de6fb7aff3d5d47d2e453
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2026-01-27 18:34:12 +0400

    Update Solidity version to latest

--- script/TrustedChain.s.sol
+++ script/TrustedChain.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.33;
 
 import {Script} from "forge-std/Script.sol";
 import {TrustedChain} from "../src/TrustedChain.sol";
